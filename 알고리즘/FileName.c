8a6950079e2dec08dcc439b88c435c74458046bb
judonghyeon <94613341+judonghyeon@users.noreply.github.com>
2024-03-07 14:17:34 +0900

    2024/03/07/목
    알고리즘 수업 파일 입니다.

---
+++ 알고리즘/FileName.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main() {
+	printf("Hello, C ! 내 이름은 주동현이고 개발 환경은 Visual Studio 입니다.");
+}
---
+++ 알고리즘/Untitled-1.py
@@ -0,0 +1,16 @@
+import turtle as t
+def bbeoljit():
+    t.shape("turtle")
+    a = 10
+    while (1):
+        a+=0.1
+        t.pendown()
+        t.forward(a)
+        t.left(10)
+        t.write("Hello World!")
+
+
+        
+def hw01():
+    print("Hello, Py 3.11.7! 내 이름은 주동현이고 개발환경은 Visual Studio Code 입니다.") 
+hw01()
